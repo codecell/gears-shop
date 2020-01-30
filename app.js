816e1f7b34ccd7e3ccd0a41c3dd8939f63bd5df1
alfred noble <ezakaphil@gmail.com>
2020-01-30 01:54:05 +0100

    fix html validator errors

--- app.js
+++ app.js
@@ -11,3 +11,39 @@ if(customerCareLine.className = 'custom-bage') {
     customerCareLine.classList.remove('custom-badge');
   })
 }
+
+// MAP READING
+var lagosLatLng = [6.45407, 3.39467];
+var mymap = L.map('motw').setView(lagosLatLng, 13);
+
+var mapboxUrl = `
+https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}
+.png?access_token={sk.eyJ1IjoidWdvbm5hIiwiYSI6ImNrM2Q4YWJ1dTEzcDAzZW14Y3AyZDhiN3QifQ
+.pi_vgMxDM09vWlwBjrVmvw}`;
+mapboxAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+var circle = L.circle([6.45407, 3.39467], {
+  color: 'grey',
+  fillColor: '#fff',
+  fillOpacity: 0.1,
+  radius: 800
+}).addTo(mymap);
+
+var Lagos = L.marker(lagosLatLng).bindPopup('This is LAGOS NAIJA')
+    
+var cities = L.layerGroup([Lagos]);
+
+var streets   = L.tileLayer(mapboxUrl, {id: 'mapbox.streets', attribution: mapboxAttribution});
+
+L.tileLayer(mapboxUrl, {
+    maxZoom: 18,
+    attribution: mapboxAttribution,
+    id: 'mapbox.streets',
+    accessToken: 'sk.eyJ1IjoidWdvbm5hIiwiYSI6ImNrM2Q4YWJ1dTEzcDAzZW14Y3AyZDhiN3QifQ.pi_vgMxDM09vWlwBjrVmvw',
+    layers: [streets, cities]
+}).addTo(mymap);
+
+L.control.scale(100, true, false, true).addTo(mymap);
+
+
+lagosLatLng.getBounds();
